7173da72f0a390a4b110ad5e95cec69835490928
johann <johannsuhr@web.de>
2017-01-13 15:28:49 +0100

    add basic player movement and drawing

--- core/src/com/wg/game/Main.java
+++ core/src/com/wg/game/Main.java
@@ -9,21 +9,30 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Player player;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-
+        player = new Player();
 		System.out.println("hello wg runner");
 	}
 
+	public void update(float deltaTime){
+	    player.update(deltaTime);
+
+    }
+
 	@Override
 	public void render () {
+	    float delta = Gdx.graphics.getDeltaTime();
+	    update(delta);
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
+		player.draw(batch);
 		batch.end();
 	}
 	
---
+++ core/src/com/wg/game/Player.java
@@ -0,0 +1,52 @@
+package com.wg.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Johann on 13.01.2017.
+ */
+public class Player {
+
+    private int height;
+    private int width;
+    private float x;
+    private float y;
+    private float acceleration = 1.02f;
+    private float velocityX = 60;
+    private float velocityY = 0;
+    private float accelerationY = 9.81f;
+    private float gravity = -9.81f;
+
+    Texture pixel;
+
+    public Player() {
+        this.height = 80;
+        this.width = 40;
+        pixel = new Texture("Pixel.jpg");
+    }
+
+    public void draw(SpriteBatch batch) {
+        batch.draw(pixel, x, y, width, height);
+    }
+
+
+    public void update(float deltaTime) {
+        //velocityX *= acceleration;
+        x += velocityX * deltaTime;
+        y += velocityY * deltaTime;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+            System.out.println("up");
+            velocityY = 60;
+        }
+
+        velocityY += gravity;
+
+        if (y <= 0) {
+            y = 0;
+        }
+    }
+}
