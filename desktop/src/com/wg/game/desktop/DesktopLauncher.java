28404d7759f02945a8f83d87c952fc4b0f321e9c
johann <johannsuhr@web.de>
2017-01-13 14:05:30 +0100

    libgdx project files added

---
+++ desktop/src/com/wg/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.wg.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.wg.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
