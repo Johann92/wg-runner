7659dce786e1021fe7d45df4b09aef60b3679f20
Franz Benthin <fbenthin@mevango.com>
2017-01-13 14:45:09 +0100

    adjust aspect ratio

--- desktop/src/com/wg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/wg/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		config.width = 640;
 		config.height = 360;
+		config.width = 640;
 
 		new LwjglApplication(new Main(), config);
 	}
